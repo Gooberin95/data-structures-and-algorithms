0ac9ee95f3a100540087a679b70735a1d0986546
Brandon Ruiz <Sober4202020@outlook.com>
2023-04-20 03:11:47 +0000

    i want to frame this lol

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -89,11 +89,14 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr, value) => {
-  // Solution code here...
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
+  for(let i = 0; i < times; i ++) {
+    callback(arr, num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -115,10 +118,18 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
+  const finlist = [];
+  availableItems.forEach(item => {
+    if(item.available === true) {
+      finlist.push(item.name);
+    }
+
+  });
+  return finlist;
 };
 
-/* ------------------------------------------------------------------------------------------------
+/* ------------------
+------------------------------------------------------------------------------
 STRETCH - CHALLENGE 7
 
 Write a function named fizzbuzz that takes in an array of numbers.
@@ -126,7 +137,8 @@ Write a function named fizzbuzz that takes in an array of numbers.
 Iterate over the array using forEach to determine the output based on several rules:
   - If a number is divisible by 3, add the word "Fizz" to the output array.
   - If the number is divisible by 5, add the word "Buzz" to the output array.
-  - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
+  - If the number is divisible by both 3 and 5,
+   add the phrase "Fizz Buzz" to the output array.
   - Otherwise, add the number to the output array.
 
 Return the resulting output array.
