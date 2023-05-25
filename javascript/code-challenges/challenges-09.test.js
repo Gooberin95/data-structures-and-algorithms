7aa37ee8a7a356a38baec6cbaf081078675da17d
Brandon Ruiz <Sober4202020@outlook.com>
2023-05-25 00:22:10 +0000

    adding

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -35,7 +35,7 @@ CHALLENGE 2
 
 Write a function named getCourseKeys that takes in the courseInfo object and returns an array containing the keys for the courseInfo object.
 
-For example: (['name', 'duration', 'topics', 'finalExam']).
+For example: (['name', 'duration', 'topics', 'finalExam'])
 ------------------------------------------------------------------------------------------------ */
 const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningTrack: '8 weeks'},
   topics: ['SMACSS', 'APIs', 'NodeJS', 'SQL', 'jQuery', 'functional programming'],
